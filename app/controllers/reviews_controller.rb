4654f552b14d34b95526c298e4c71725db8f95e4
Cliffsky <nickvbn@gmail.com>
2016-11-26 03:05:33 +0000

    fixed reviews error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,14 +3,16 @@ class ReviewsController < ApplicationController
   before_filter :authorize
 
   def create
+    user_id = User.find_by_id(session[:user_id])
     @product = Product.find(params[:product_id])
-    @CheckForReview = Review.where('user_id = ? AND product_id = ?', params[:user_id], params[:product_id])
-    puts @CheckForReview
-    # if @CheckForReview
-    #   redirect_to product_path(@product), notice: 'User already commented on this product'
-    # else
-    #   @review = @product.reviews.create(review_params)
-    # end
+    @CheckForReview = Review.where('user_id = ? AND product_id = ?', user_id, params[:product_id])
+    pp @CheckForReview
+    if @CheckForReview.length != 0
+      redirect_to product_path(@product), notice: 'User already commented on this product'
+    else
+      @review = @product.reviews.create(review_params)
+      redirect_to product_path(@product)
+    end
   end
 
   def destroy
